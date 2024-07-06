c72c1dee61426371526cae6dc8766f918e0652e5
Richard Körösi <korosi.richard@gmail.com>
2024-07-06 11:30:29 +0200

    feat: add crossorigin to ElevatorController for communication with frontend

--- BackendSpringBoot/src/main/java/org/solver/backendspringboot/controllers/ElevatorController.java
+++ BackendSpringBoot/src/main/java/org/solver/backendspringboot/controllers/ElevatorController.java
@@ -2,6 +2,7 @@ package org.solver.backendspringboot.controllers;
 
 import org.solver.backendspringboot.objects.Elevator;
 import org.solver.backendspringboot.objects.Floor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class ElevatorController {
     Elevator elevator;
     List<Floor> floors;
